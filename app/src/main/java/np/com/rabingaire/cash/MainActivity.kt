3bf87623c707e2e187c47f4bba9ba8d7885fd887
Rabin Gaire <rabingaire20@gmail.com>
2018-06-02 19:16:51 +0545

    camerakit added to the project

---
+++ app/src/main/java/np/com/rabingaire/cash/MainActivity.kt
@@ -0,0 +1,23 @@
+package np.com.rabingaire.cash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        camera.start()
+    }
+
+    override fun onPause() {
+        camera.stop()
+        super.onPause()
+    }
+}
