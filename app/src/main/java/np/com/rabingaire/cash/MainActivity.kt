ff3ff09837553802142bef19a592de11ab9f640e
Rabin Gaire <rabingaire20@gmail.com>
2018-06-02 19:47:39 +0545

    onclick button function added

--- app/src/main/java/np/com/rabingaire/cash/MainActivity.kt
+++ app/src/main/java/np/com/rabingaire/cash/MainActivity.kt
@@ -2,6 +2,8 @@ package np.com.rabingaire.cash
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onCaptureClick(view: View) {
     }
 
     override fun onResume() {
